8779daa1c6ae0cae2dab2db7bf39e510aabbb07e
Mikex00001 <controletotal1043@gmail.com>
2023-02-09 23:30:43 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var counter, min = 19,
+    sec = 60;
+
+function timer() {
+    0 === sec ? (min--, sec = 59) : sec--, min < 0 ? clearInterval(counter) : 
+    document.getElementById("timer").innerHTML = "ESTE VIDEO VAI EXPIRAR EM " + (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec) + " !!!!";
+    if(min == 00 && sec == 00){
+        document.getElementById("timer").innerHTML = "ADIQUIRA AGORA !!!";
+    }
+}
+window.onload = function() {
+    counter = setInterval(timer, 1e3)
+};
